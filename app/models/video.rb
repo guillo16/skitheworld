b96d2f428a5f4c929c38d7f1412f99114dddcfe8
guillermo varela <guillovarela16@gmail.com>
2018-12-28 15:57:40 -0300

    title corrected

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ApplicationRecord
-  validates :name, presence: true
+  validates :title, presence: true
   validates :url, uniqueness: true, presence: true
   validates :rating, presence: true
   validates :region, presence: true
