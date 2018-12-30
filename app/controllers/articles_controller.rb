a73bab3f7e2254ccfc3e59c519974f1d786a56dc
guillermo varela <guillovarela16@gmail.com>
2018-12-30 13:38:51 -0300

    finish show articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_article, only: [:show]
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :videos, only: [:index, :show]
   resources :skis, only: [:index, :show]
+  resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
