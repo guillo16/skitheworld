edfba4b8471c1a8a7649763bdb53bb01d847263e
guillermo varela <guillovarela16@gmail.com>
2019-01-03 20:44:56 -0300

    finish bookings

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,9 +2,9 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr("#range_start", {
+flatpickr("#start_date", {
   altInput: true,
-  plugins: [new rangePlugin({ input: "#range_end"})]
+  plugins: [new rangePlugin({ input: "#end_date"})]
 })
 
 
