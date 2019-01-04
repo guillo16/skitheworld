80a0e680e50ee407d651f4afc7442e37892fb42b
guillermo varela <guillovarela16@gmail.com>
2019-01-04 12:27:22 -0300

    add geocoder migration to resorts

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,4 +1,6 @@
 class Resort < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :bookings, dependent: :destroy
   validates :name, presence: true
   validates :description, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_182515) do
+ActiveRecord::Schema.define(version: 2019_01_04_152452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,9 @@ ActiveRecord::Schema.define(version: 2019_01_03_182515) do
     t.string "tips"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
   end
 
   create_table "skis", force: :cascade do |t|
