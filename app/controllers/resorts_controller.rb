afbc75d6a62a544ab8273dbc2def0b2cabd9fa82
guillermo varela <guillovarela16@gmail.com>
2019-01-04 15:12:05 -0300

    finish map style

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -3,6 +3,18 @@ class ResortsController < ApplicationController
   before_action :set_resorts, only: [:show]
 
   def index
+    @resorts = Resort.where.not(latitude: nil, longitude: nil)
+
+    @markers = @resorts.map do |resort|
+      {
+        lat: resort.latitude,
+        lng: resort.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+        # Uncomment the above line if you want each of your markers to display a info window when clicked
+        # (you will also need to create the partial "/flats/map_box")
+      }
+    end
+
     @resorts = Resort.all
     if params["region"]
       @resorts = Resort.where(region: params["region"])
--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,9 +1,9 @@
 class Resort < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
   has_many :bookings, dependent: :destroy
   validates :name, presence: true
   validates :description, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   def available?(requested_start_date, requested_end_date)
     bookings.each do |booking|
